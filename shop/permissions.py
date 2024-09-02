6c6d5db8bbbfc6e8892bedfec2c2094eaab65333
Omar <omarkhaled1752003@yahoo.com>
2024-09-02 19:09:51 +0300

    all methods requires the owner

--- shop/permissions.py
+++ shop/permissions.py
@@ -15,6 +15,4 @@ class IsAuthenticatedOrAdmin(permissions.BasePermission):
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
         return request.user == obj.user
