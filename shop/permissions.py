fd2f7e540c938d3fb7666b6c7f8a9bbf7124837f
Omar <omarkhaled1752003@yahoo.com>
2024-09-02 15:03:48 +0300

    customer permission for users who placed an order

--- shop/permissions.py
+++ shop/permissions.py
@@ -11,3 +11,10 @@ class IsAuthenticatedOrAdmin(permissions.BasePermission):
             (request.user and request.user.is_authenticated)
             or (request.user and request.user.is_staff)
         )
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj.user or request.user.profile.user_type == "admin"
