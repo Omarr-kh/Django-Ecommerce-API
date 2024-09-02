cacb81a65135113e81f5cf04ec86c156649622af
Omar <omarkhaled1752003@yahoo.com>
2024-09-02 17:48:28 +0300

    modified OrderSerializer to include order items

--- shop/serializers.py
+++ shop/serializers.py
@@ -8,13 +8,31 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = "__all__"
-
-
 class OrderItemSerializer(serializers.ModelSerializer):
+    product_name = serializers.SerializerMethodField()
+
     class Meta:
         model = OrderItem
-        fields = "__all__"
+        fields = ["id", "product_name", "quantity", "order_id", "product_id"]
+
+    def get_product_name(self, obj):
+        return obj.product.name
+
+    def get_product_id(self, obj):
+        return obj.product.id
+
+    def get_order_id(self, obj):
+        return obj.order.id
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ["id", "status", "created_at", "updated_at", "items"]
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        validated_data["user"] = user
+        return super().create(validated_data)
