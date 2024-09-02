7c0187ca22aef39717dbc15b10f116b15c8d13ab
Omar <omarkhaled1752003@yahoo.com>
2024-09-02 15:59:53 +0300

    default permission

--- ECommercePlatform/settings.py
+++ ECommercePlatform/settings.py
@@ -33,9 +33,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
     ),
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "shop.permissions.IsAuthenticatedOrAdmin",
-    # ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "shop.permissions.IsAuthenticatedOrAdmin",
+    ],
 }
 
 INSTALLED_APPS = [
