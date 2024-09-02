a1e320dd320468706d20f7be4f44712726616cea
Omar <omarkhaled1752003@yahoo.com>
2024-09-02 16:00:13 +0300

    login class view

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework import permissions, status
 from rest_framework.response import Response
-from rest_framework import status
+
+from .models import Product, Order, OrderItem
 
 
 @api_view(["POST"])
+@permission_classes([permissions.AllowAny])
 def register_user(request):
     if request.method == "POST":
         username = request.data.get("username")
@@ -30,4 +34,17 @@ def register_user(request):
         )
         Token.objects.create(user=user)
 
-        return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(
+            {"message": "User registered successfully!"}, status=status.HTTP_201_CREATED
+        )
+
+
+class CustomLogin(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
