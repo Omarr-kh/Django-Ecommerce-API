d36711b54ea684ee66d29303dadf6f5ac05df91b
Omar <omarkhaled1752003@yahoo.com>
2024-09-02 15:01:53 +0300

    registering models to admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Order, OrderItem
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "price", "stock")
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("user", "status")
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "product", "quantity")
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
