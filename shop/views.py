0363a7cf442ca31dd03f2c830e754715d4961c91
Omar <omarkhaled1752003@yahoo.com>
2024-09-02 15:33:13 +0300

    registration view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@api_view(["POST"])
+def register_user(request):
+    if request.method == "POST":
+        username = request.data.get("username")
+        password = request.data.get("password")
+        email = request.data.get("email")
+
+        if not username or not password or not email:
+            return Response(
+                {"error": "Username, Password and email are required!"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if User.objects.filter(username=username).exists():
+            return Response(
+                {"error": "Username already exists!!"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user = User.objects.create(
+            username=username, email=email, password=make_password(password)
+        )
+        Token.objects.create(user=user)
+
+        return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
