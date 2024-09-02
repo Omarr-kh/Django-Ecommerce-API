1b768cb3d1a52e7bba2ee1af29958d43a25aed4d
Omar <omarkhaled1752003@yahoo.com>
2024-09-02 15:02:18 +0300

    custom permission for authenticated users or admins

---
+++ shop/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class IsAuthenticatedOrAdmin(permissions.BasePermission):
+    """
+    Allows access only to authenticated users or admins.
+    """
+
+    def has_permission(self, request, view):
+        return bool(
+            (request.user and request.user.is_authenticated)
+            or (request.user and request.user.is_staff)
+        )
