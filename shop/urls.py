bcb0801bdfe1ac51125d39356c78fd3dc63fd082
Omar <omarkhaled1752003@yahoo.com>
2024-09-02 16:28:11 +0300

    renamed class view

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("register", views.register_user, name="register-user"),
     path("login", views.CustomLogin.as_view(), name="login"),
-    path("products", views.ListProductsView.as_view(), name="list-products"),
+    path("products", views.ProductListCreateView.as_view(), name="list-products"),
 ]
